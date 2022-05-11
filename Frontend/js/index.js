6c2c0d6454fc320063e5ed890d0582835eedee01
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-05-11 17:24:37 +0200

    code modifs

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -13,7 +13,7 @@ const displayBooks = (books) => {
     html += '<div class="card-body">';
     html += `<h4 class='card-title'>Titre : ${book.title}</h5>`;
     html += `<h5 class='card-title'>Author : ${book.author}</h5>`;
-    html += `<a href="editBook.html?id=${book.id_book}" class="btn btn-primary">Go somewhere</a>`;
+    html += `<a href="editBook.html?id=${book.id_book}" class="btn btn-primary">Edit</a>`;
     html += " </div></div></div>";
     return html;
   });
